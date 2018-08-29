50ae976a489220155a4eb3c81c59f2afb6cb4b96
Kareem James <kareemdjames@gmail.com>
2018-08-28 22:32:52 -0400

    created initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: './index.html'
+})
+
+const PORT = process.env.PORT || 3000
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+     {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+     },
+     {
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'style-loader'
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: '[name]_[local]_[hash:base64]',
+            sourceMap: true,
+            camelCase: true,
+            minimize: true
+          }
+        }
+      ]
+     }
+    ]
+  },
+  plugins: [
+    htmlWebpackPlugin
+  ],
+  devserver: {
+    host: 'localhost',
+    port: PORT,
+    historyApiFallback: true,
+    open: true
+  }
+
+
+
+
+
+
+}
