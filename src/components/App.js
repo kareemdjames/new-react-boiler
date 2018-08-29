9f67a222cd89cfc79abee81b4c08d794cbb97738
Kareem James <kareemdjames@gmail.com>
2018-08-28 23:03:37 -0400

    created initial App.js

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+     <div className='app'>
+       <h1>Welcome Kareem, from REACT!!</h1>
+     </div>
+    )
+  }
+}
+
+export default App
